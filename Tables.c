25ef34ff5ef321943cadb944cc95c441252c0a37
Santhosh1757 <125586863+Santhosh1757@users.noreply.github.com>
2023-02-18 09:00:37 +0530

    Assignment 3

---
+++ Tables.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,i=1,c;
+    printf("Enter the Number:");
+    scanf("%d",&a);
+    while(i<=10)
+    {
+    c=a*i;
+    printf("\n%d * %d = %d\n",a,i,c);
+    i++;
+    }
+    
+    return 0;
+}
+
+    
