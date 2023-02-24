db0d3962138fcdbc8b4544da7e84345dfc769609
Santhosh1757 <125586863+Santhosh1757@users.noreply.github.com>
2023-02-24 22:16:48 +0530

    Assignment 1

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int i,j,k;
+    for(i=1;i<=7;i=i+2){
+    printf("%d ",i);
+    }
+    printf("\n");
+    for(j=5;j<=11;j=j+2){
+    printf("%d ",j);
+    }
+    printf("\n");
+    for(k=9;k<=15;k=k+2){
+    printf("%d ",k);
+    }
+    return 0;
+}
